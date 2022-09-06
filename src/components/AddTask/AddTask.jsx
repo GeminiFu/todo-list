7179e3ac3efcecefedab0cdc0e695ca0895b3234
Albus Fu <b123963@yahoo.com.tw>
2022-09-06 19:15:39 +0800

    add task

---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from './index.css'
+
+const AddTask = function() {
+
+
+
+
+    return (
+        <div className={styles.wrapper}>
+            <p className={styles.text}>+ Add Task</p>
+        </div>
+    )
+}
+
+export default AddTask
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import styles from './index.css'
 
 import Nav from './components/Nav'
+import AddTask from "./components/AddTask";
+
 
 ReactDOM.render(
 
   <div className={styles['body-wrapper']}>
     <div className={styles['body']}>
       <Nav />
+      <AddTask />
     </div>
   </div>,
   document.getElementById('root')
